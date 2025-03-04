f452bcfad73b6bf0a571933d3d31e163abe6c27f
aleksei z <farguzalexey@gmail.com>
2025-03-04 14:17:54 +0300

    change code structure, add github action

--- Makefile
+++ Makefile
@@ -12,7 +12,18 @@ package-uninstall:
 	uv tool uninstall hexlet-code
 
 gendiff:
-	uv run gendiff gendiff/test_files/file1.json gendiff/test_files/file2.json
+	uv run gendiff gendiff/tests/test_files/file1.json gendiff/tests/test_files/file2.json
 
 lint:
 	uv run ruff check gendiff
+
+lint-with-fix:
+	uv run ruff check --fix gendiff
+
+test:
+	uv run pytest
+
+test-coverage:
+	uv run pytest --cov=hexlet_python_package --cov-report xml
+
+check: test lint
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,43 +1,12 @@
-import argparse
+from gendiff.parser import parsing
+from gendiff.generate_diff_tree import generate_diff
 import json
-from collections import OrderedDict
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    parser.add_argument("-f", "--format", help="set format of output")
-    args = parser.parse_args()
-
+    args = parsing()
     generate_diff(args.first_file, args.second_file)
     
-    
-
-def generate_diff(file_path1: str, file_path2: str) -> str:
-    first_file = json.load(open(file_path1))
-    second_file = json.load(open(file_path2))
-
-    res = {}
-
-    for k1 in first_file.keys():
-        if k1 in second_file.keys():
-            if first_file[k1] == second_file[k1]:
-                res['  ' + k1] = first_file[k1]
-            else:
-                res['- ' + k1] = first_file[k1]
-                res['+ ' + k1] = second_file[k1]
-        else:
-            res['- ' + k1] = first_file[k1]
-
-    for k2, v2 in second_file.items():
-        if k2 not in first_file.keys():
-            res['+ ' + k2] = second_file[k2]
-
-    res_sorted = dict(sorted(res.items(), key=lambda x: x[0][2:]))
-    res_sorted_json = json.dumps(res_sorted)
-    print(res_sorted_json)
-    return res_sorted_json
 
 if __name__ == '__main()__':
     main()
