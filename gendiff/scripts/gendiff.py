6ca94f73ecb2e821a09f4da0d9aba38216315e4c
aleksei z <farguzalexey@gmail.com>
2025-03-21 15:20:38 +0300

    fix #4 main func

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,16 +3,15 @@ from gendiff.generate_diff_tree import generate_diff_tree
 from gendiff.parser import parsing
 
 
-def generate_diff():
+def generate_diff(first_file: str, second_file: str, format: str='stylish'):
     
-    diff_tree = generate_diff_tree(args.first_file, args.second_file, args.format)
-    print(choose_format(diff_tree, args.format))
+    diff_tree = generate_diff_tree(first_file, second_file, format)
+    print(choose_format(diff_tree, format))
 
 
 def main():
-    global args
     args = parsing()
-    generate_diff()
+    generate_diff(args.first_file, args.second_file, args.format)
     
 
 if __name__ == '__main()__':
