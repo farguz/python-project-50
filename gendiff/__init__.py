45599c65b54d2bad9803e28411ff04099fc95e0a
aleksei z <farguzalexey@gmail.com>
2025-03-21 08:50:35 +0300

    fix init file once again

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -3,9 +3,12 @@ from gendiff.formatter import json_format as json_format
 from gendiff.formatter import plain as plain
 from gendiff.formatter import stylish as stylish
 from gendiff.generate_diff_tree import generate_diff_tree as generate_diff_tree
+from gendiff.scripts.gendiff import generate_diff as generate_diff
+
 
 __all__ = ('generate_diff_tree',
            'json_format',
            'stylish',
            'choose_format',
-           'plain')
+           'plain',
+           'generate_diff')
